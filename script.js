4979cbe70a38686cd8bd158366ec0461bd9dab15
mwlluis <mwlluis@hotmail.com>
2022-03-24 20:37:15 -0400

    checks for winning combinations

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
-
+const WINNING_COMBINATIONS = [
+  [0, 1, 2], 
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn
 
+startGame()
 
-cellElements.forEach(cell => {
+
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
   cell.addEventListener('click', handleClick, { once: true })
 })
+  setBoardHoverClass()
+}
 
 function handleClick(e) {
   const cell = e.target
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
   placeCharacter(cell, currentClass)
+  if (checkWin(currentClass)) {
+      endGame(false)
+  }  
   // check for win
   // check for draw
   // switch turns
   swapTurns()
   setBoardHoverClass()
+}
+
+function endGame(draw) {
+  if (draw) {
+
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!` 
+  }
+
+    winningMessageTextElement.classList.add('show');
+
 }
 
 function placeCharacter(cell, currentClass) {
@@ -37,4 +68,12 @@ function setBoardHoverClass() {
   } else {
     board.classList.add(X_CLASS)
   }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
 }
